9e56924d710cc55af6d18a34a27d835045ee2133
minhlephu <110329077+minhlephu@users.noreply.github.com>
2023-01-09 17:37:46 +0700

    MinhlP

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -4,11 +4,20 @@ import MovieCard from "../components/movie/MovieCard";
 import MovieList from "../components/movie/MovieList";
 import { fetcher } from "../config";
 import useDebounce from "../hooks/useDebounce";
+import ReactPaginate from "react-paginate";
 //https://api.themoviedb.org/3/search/company?api_key=<<api_key>>
+
+const pageCount = 5;
+const itemsPerPage = 20;
 const MoviePage = () => {
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
   const [filter, setFilter] = useState("");
+  const [nextPage, setNextPage] = useState(1);
+
   const [url, setUrl] = useState(
-    "https://api.themoviedb.org/3/movie/popular?api_key=cdfae351f75567739fbe7ec0008bc139"
+    `https://api.themoviedb.org/3/movie/popular?api_key=cdfae351f75567739fbe7ec0008bc139&page=${nextPage}`
   );
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
@@ -21,16 +30,27 @@ const MoviePage = () => {
   useEffect(() => {
     if (filterDebounce) {
       setUrl(
-        `https://api.themoviedb.org/3/search/movie?api_key=cdfae351f75567739fbe7ec0008bc139&query=${filterDebounce}`
+        `https://api.themoviedb.org/3/search/movie?api_key=cdfae351f75567739fbe7ec0008bc139&query=${filterDebounce}&page=${nextPage}`
       );
     } else {
       setUrl(
-        "https://api.themoviedb.org/3/movie/popular?api_key=cdfae351f75567739fbe7ec0008bc139"
+        `https://api.themoviedb.org/3/movie/popular?api_key=cdfae351f75567739fbe7ec0008bc139&page=${nextPage}`
       );
     }
-  }, [filterDebounce]);
+  }, [filterDebounce, nextPage]);
   const movies = data?.results || [];
-  console.log(movies);
+
+  useEffect(() => {
+    if (!data || !data.total_pages) return;
+    setPageCount(Math.ceil(data.total_pages / itemsPerPage));
+  }, [data, itemOffset]);
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % data.total_pages;
+    setItemOffset(newOffset);
+    setNextPage(event.selected + 1);
+  };
+
   return (
     <div className="py-10 page-container">
       <div className="flex mb-10 ">
@@ -69,44 +89,17 @@ const MoviePage = () => {
             <MovieCard key={item.id} item={item}></MovieCard>
           ))}
       </div>
-
-      <div className="flex items-center justify-center mt-10 gap-x-5">
-        <span className="cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15.75 19.5L8.25 12l7.5-7.5"
-            />
-          </svg>
-        </span>
-        <span className="inline-block px-4 py-2 leading-none bg-white rounded cursor-pointer text-slate-900">
-          1
-        </span>
-
-        <span className="cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M8.25 4.5l7.5 7.5-7.5 7.5"
-            />
-          </svg>
-        </span>
+      <div className="mt-10">
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+          className="pagination"
+        />
       </div>
     </div>
   );
